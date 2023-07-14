1d0fe2a753b18ad7e0df87b02bafa697343ab9e8
ridhoadilla <bd3alz@gmail.com>
2023-07-14 14:41:28 +0700

    ubah variable DT build version berdasar kan tanggal - jam

--- src/PartsUnlimitedWebsite/Program.cs
+++ src/PartsUnlimitedWebsite/Program.cs
@@ -15,7 +15,7 @@ namespace PartsUnlimited
             var host = BuildWebHost(args).Build();
             // Set environment variables
             DateTime currentDateTime = DateTime.Now;
-            string formattedDateTime = currentDateTime.ToString("dddd, dd MMMM yyyy");
+            string formattedDateTime = currentDateTime.ToString("dddd, dd MMMM yyyy HH:mm:ss");
             string releaseVersion = "1.0";
             string buildVersion = formattedDateTime;
             string releaseProduct = "AGIT-DCCS";
